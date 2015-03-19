2ebf6b03eef538ebc726e7d732f30b4ecb2817d8
nakazy <nakazy@nakazy-no-iMac.local>
2015-03-20 01:02:58 +0900

    Initial Commit

---
+++ TestFacebook/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TestFacebook
+//
+//  Created by nakazy on 2015/03/20.
+//  Copyright (c) 2015年 nakazy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
